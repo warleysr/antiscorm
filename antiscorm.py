d510095cd3327da2a8081f43cbb64c11342469ea
Warley <warleysoaresr@gmail.com>
2022-07-19 20:27:32 -0300

    adicionado menu

--- antiscorm.py
+++ antiscorm.py
@@ -9,6 +9,7 @@ from ctypes import windll
 import PySimpleGUI as Sg
 import json
 import re
+import os
 
 
 antiscorm = {}
@@ -16,7 +17,14 @@ antiscorm = {}
 
 def start_interface():
     gen_font = "Arial 12 bold"
+
+    menu_opt = [
+        ["Opções", ["Inserir imagens no PDF final"]],
+        ["Sobre", ["Sobre o AntiScorm"]],
+    ]
+
     layout = [
+        [Sg.Menu(menu_opt)],
         [
             Sg.Push(),
             Sg.Text("AntiScorm", text_color="lightgreen", font="Arial 22 bold"),
@@ -47,7 +55,7 @@ def start_interface():
             url = values[0]
             filepath = values[1]
             url_pattern = "^https?:\\/\\/(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)$"
-            if re.match(url_pattern, url) is None:
+            if url is None or re.match(url_pattern, url) is None:
                 Sg.PopupError("O link informado não é uma URL válida.", font=gen_font)
             elif filepath == "Nenhum selecionado":
                 Sg.PopupError(
@@ -70,6 +78,47 @@ def start_interface():
                         "Erro no arquivo de configuração. Fale com o criador do mesmo.",
                         font=gen_font,
                     )
+        elif event == "Inserir imagens no PDF final":
+            filepath = Sg.PopupGetFile(
+                "Selecione o PDF gerado pelo AntiScorm",
+                font=gen_font,
+                file_types=[("PDF do Scorm", ".pdf")],
+            )
+            if filepath is None:
+                continue
+            elif filepath == "":
+                Sg.PopupError("Nenhum arquivo foi selecionado", font=gen_font)
+                continue
+
+            folderpath = Sg.PopupGetFolder(
+                "Selecione a pasta contendo as imagens", font=gen_font
+            )
+            if folderpath is None:
+                continue
+            elif folderpath == "":
+                Sg.PopupError("Nenhuma pasta foi selecionada", font=gen_font)
+                continue
+
+            folder = os.listdir(folderpath)
+            img_count = 0
+            for file in folder:
+                if file.endswith(".png") or file.endswith(".jpg"):
+                    img_count += 1
+            if len(folder) == 0 or img_count == 0:
+                Sg.PopupError("A pasta selecionada não contém imagens.", font=gen_font)
+                continue
+
+            print("Iniciando processo de colocar imagens")
+        elif event == "Sobre o AntiScorm":
+            Sg.PopupOK(
+                "Versão: 1.0.0\n\nO AntiScorm foi criado com o objetivo de facilitar a"
+                + " realização das atividades SCORM do IFMG Betim.\n\nO objetivo do AntiScorm NÃO é"
+                + " promover o uso inconsciente de informações mas facilitar o trabalho manual de quem"
+                + " já compreendeu o assunto.\n\nA principal vantagem é a geração automática do relatório"
+                + "em PDF no formato requerido.",
+                title=event,
+                font=gen_font,
+            )
 
     window.close()
 
